e8944b19fc2548efaa10e5be1c7343df8343d01e
admin <iamsofi2021@gmail.com>
2021-03-30 17:18:02 +0300

    added files

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(express.static(`${__dirname}/client/dist/public`));
+
+//API
+app.get('*', function(req, res) {
+    res.sendFile(`${__dirname}/client/dist/public/index.html`);
+});
+
+const port = process.env.port || 8000;
+app.listen(port, () => {
+    console.log(`Express server listening on port ${port}`);
+});
